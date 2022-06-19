c2ce6baf011dd3b61b8dab5beda67f25c77b2f1b
Andrzej Bończyszyn <baczek1133@gmail.com>
2022-06-19 14:58:47 +0200

    Edycja 2

--- Plik1.java
+++ Plik1.java
@@ -1,5 +1,5 @@
 public class Plik1 {
     public static void main(String[] args) {
-        System.out.println("Hello World edycja 1");
+        System.out.println("Hello World edycja 1 edycja 2");
     }
 }
